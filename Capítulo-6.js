a2c77f622086edcf0fb56715ed06e73e0ee303b6
bgamas <bryangaso15@gmail.com>
2021-04-12 14:01:12 -0500

    feat: + capítulo 6

---
+++ Capítulo-6.js
@@ -0,0 +1,98 @@
+/* 1. Un tipo vector */
+class Vec {
+    constructor(x, y){ 
+        this.x = x;
+        this.y = y;
+    }
+    
+    mas(vb){
+        return new Vec(this.x + vb.x, this.y + vb.y)
+    }
+
+    menos(vb){
+        return new Vec(this.x - vb.x, this.y - vb.y)
+    }
+
+    get longitud(){
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+    }
+}
+console.log(new Vec(1, 2).mas(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).menos(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).longitud);
+// → 5
+
+
+/* 2. Conjuntos */
+class Group {
+    constructor(){
+        this.conjunto = []
+    }
+
+    add(e){
+        if(!this.conjunto.includes(e))
+            this.conjunto.push(e);
+    }
+
+    delete(e){
+        let index = this.conjunto.indexOf(e);
+        if( index >= 0)
+          this.conjunto.splice(index, 1)
+    }
+
+    has(e){
+        return this.conjunto.includes(e);
+    }
+
+    static from(iter){
+        let conjunto = new Group();
+        for (let e of iter){
+            conjunto.add(e)
+        }
+        return conjunto
+    }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
+
+
+
+/* 3. Conjuntos Iterables */
+// Al código del ejercicio 2, añadir lo siguiente:
+class GroupIterator {
+    constructor(conjunto){
+        this.index = 0;
+        this.conjunto = conjunto;
+    }
+    
+    next(){
+        if(this.index == this.conjunto.conjunto.length)
+            return {done: true}
+        let value = this.conjunto.conjunto[this.index]
+        this.index++
+        return { value, done: false }
+    }
+}
+
+Group.prototype[Symbol.iterator] = function() {return new GroupIterator(this)}
+
+
+
+/* 4. Tomando un método prestado */
+let map = {one: true, two: true, hasOwnProperty: true};
+
+hasOwnPropertySymbol = Symbol("hasOwnProperty")
+map[hasOwnPropertySymbol] = Object.prototype.hasOwnProperty
+
+console.log(map[hasOwnPropertySymbol]("one"));
+// → true
